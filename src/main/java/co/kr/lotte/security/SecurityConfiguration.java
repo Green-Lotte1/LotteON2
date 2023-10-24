c818b98537c26c702498b2d8b1baaa8f88c2a9f8
sloth <slothfkdla@gmail.com>
2023-10-24 13:41:41 +0900

    yml 및 파일 수정

--- src/main/java/co/kr/lotte/security/SecurityConfiguration.java
+++ src/main/java/co/kr/lotte/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package co.kr.lotte.security;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
@@ -25,19 +26,29 @@ public class SecurityConfiguration implements WebMvcConfigurer {
 	@Autowired
 	private ResourceLoader resourceLoader;
 
+	@Value("${filePath.files}")
+	private String files;
+	@Value("${filePath.thumbs}")
+	private String thumbs;
+	@Value("${filePath.banners}")
+	private String banners;
+
+	public SecurityConfiguration() {
+	}
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/files/**")
-				//서버용 .addResourceLocations(resourceLoader.getResource("file:/C:/Users/Java/Desktop/Workspace/LotteON2/files/")
-				.addResourceLocations(resourceLoader.getResource("file:/home/LotteON/files"));
+				//서버용 .addResourceLocations(resourceLoader.getResource("file:/C:/Users/JAVA2/Desktop/Workspace/lotte/files/")
+				.addResourceLocations(resourceLoader.getResource(files));
 
 		registry.addResourceHandler("/thumbs/**")
 				// 서버용 .addResourceLocations(resourceLoader.getResource("file:/C:/Users/Java/Desktop/Workspace/LotteON2/files/")
-				.addResourceLocations(resourceLoader.getResource("file:/home/LotteON/thumbs/"));
+				.addResourceLocations(resourceLoader.getResource(thumbs));
 
 		registry.addResourceHandler("/banners/**")
 				// 서버용 .addResourceLocations(resourceLoader.getResource("file:/C:/Users/Java/Desktop/Workspace/LotteON2/files/")
-				.addResourceLocations(resourceLoader.getResource("file:/home/LotteON/banners/"));
+				.addResourceLocations(resourceLoader.getResource(banners));
 	}
 
 	@Bean
